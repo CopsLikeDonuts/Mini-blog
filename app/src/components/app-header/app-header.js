5cd02301b575e88a9f2376322271d137346365ed
CopsLikeDonuts <reggisen@gmail.com>
2020-11-22 14:28:05 +0200

    version 1.0

---
+++ app/src/components/app-header/app-header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app-header.css';
+
+
+
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Oleksandr Tarapata</h1>
+            <h2>{allPosts} twits, {liked} liked</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
